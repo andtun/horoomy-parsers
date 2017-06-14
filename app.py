906ab3eeffef38d0aca79c471ddb4d56de39e787
Андрей Тюняткин < >
2017-06-14 23:07:01 +0300

    new parser + dynamic parsers

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ PARSER_LIST = ['cian', 'realEstate']
 
 
 def main_page():
-	div = ""
-	for p in PARSER_LIST:
-		div += """<div>
-		<span style="margin-right: 20%; margin-left: 3%;">{0}:</span><span id="{0}" style="margin-right: 17%;">xtnf</span><button onclick="window.location = '/res/{0}';">Просмотреть результаты</a>
+    div = ""
+    for p in PARSER_LIST:
+	div += """<div>
+	<span style="margin-right: 20%; margin-left: 3%;">{0}:</span><span id="{0}" style="margin-right: 17%;">xtnf</span><button onclick="window.location = '/res/{0}';">Просмотреть результаты</a>
 	</div>""".format(p)
-	return template("./html/main.html", plist = str(PARSER_LIST), parserdivs = div)
+    return template("./html/main.html", plist = str(PARSER_LIST), parserdivs = div)
 
 
 @get("/")
@@ -22,7 +22,7 @@ def main():
     return main_page()
 
 @get("/start_parse")
-def st():
+def st():              
     maxprice = request.query.maxprice
     for parser_name in PARSER_LIST:
     	t = threading.Thread(target = parse_it, args=(parser_name, maxprice,))
