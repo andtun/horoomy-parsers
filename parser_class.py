0936441fbf746f4864eb2aef34889d4f0fd61283
Andrey Tunyatkin <andtun@yandex.ru>
2017-06-19 15:40:31 +0300

    new db logic

--- parser_class.py
+++ parser_class.py
@@ -95,7 +95,7 @@ NULL,
 "%s",
 '%s',
 );
-""" % (data['cost'], data['room_num'], data['area'], data['contacts']['phone'], data['date'], 'NULL', json.dumps(data['pics']), json.dumps(data['contacts']), data['descr'].encode('utf-8'), data['adr'], json.dumps(data['metro']), data['url'], data['loc'])
+""" % (data['cost'], data['room_num'], data['area'], data['contacts']['phone'], data['date'], 'NULL', json.dumps(data['pics']), json.dumps(data['contacts']), data['descr'], data['adr'], json.dumps(data['metro']), data['url'], data['loc'])
         print(cmnd)
         self.db.query(cmnd)
 
