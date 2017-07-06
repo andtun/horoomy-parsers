6491cf904666f413c1d9f72113bb9fbf432acbe0
Andrey Tunyatkin <andtun@yandex.ru>
2017-07-06 12:09:02 +0300

    only msk realEstate

--- parseAPI.py
+++ parseAPI.py
@@ -77,12 +77,13 @@ def cian(maxprice):
         p.save_results(x)
         
     print("ALL INFA WRITTEN")
-    p.add_date() #я не умею эту фигню писать
+    p.add_date()
 
 
 # ===========================================================================================================
 
 def realestate(maxprice):
+    
     def get_html(url):
         r = requests.get(url)
         print(str(r), end=' ')
@@ -108,22 +109,27 @@ def realestate(maxprice):
         try:
             metro = []
             metro.append(info.find('div', class_='object-info-link_l1').find('a').text)
-
         except:
             metro = []
 
         #Address
         # try:
-        adr = ''
-        elms = info.find('div', class_='object-info-link_l2').find_all('a')
-        for elm in elms:
-            adr += elm.text + ' '
+        # adr = ''
+        # elms = info.find('div', class_='object-info-link_l2')#.find_all('a')
+        # # for elm in elms:
+        # #     print(elm)
+        # #     adr += elm.text + ' '
+        # print(elms)       
     # except:
     #   adr = '-'       
 
         #Created date
         date = media.find('div', class_='obj-info-dop').text.split()[1][:-1]
+        date = date.split('.')
+        date[2] = '20' + str(date[2])
+        date = '.'.join(date)
             #date = '-'
+        
 
 
         temp = info.find('div', class_='object-params').find('div',class_='params-block').find_all('div', class_='params-item')
@@ -164,23 +170,36 @@ def realestate(maxprice):
         #Contacts
 
         contacts = {'vk': "", 'fb': "", 'email': "", 'phone': ""}
-        try:
-            add = int(media.find('div', class_='obj-active-panel').find('div', class_='toogle-button').get('blst'))
-            if add == 0:
-                add = int(media.find('div', class_='obj-active-panel').find('div', class_='toogle-button').get('lst1'))
-            add = str(add//17)[1:]  
-            phone = media.find('div',class_='object-connect').find('div',class_='object-builder-phone_block').find('div', class_='object-builder-phone').text[:-3] + add
-            print(phone)
-            contacts['phone'] = phone
-        except:
-            contacts['phone'] = 'err'
+        add = int(media.find('div', class_='obj-active-panel').find('div', class_='toogle-button').get('blst'))
+        if add == 0:
+            add = int(media.find('div', class_='obj-active-panel').find('div', class_='toogle-button').get('lst1'))
+        add = str(add//17)[1:]  
+        phone = media.find('div',class_='object-connect').find('div',class_='object-builder-phone_block').find('div', class_='object-builder-phone').text[:-3] + add
+        #print(phone)
+        contacts['phone'] = phone
 
 
         #loc
+        # loc = []
+        # if not adr:
+        #   headers = {
+        #       'Referer': 'http://www.realestate.ru/flatrent/4320916/',
+        #       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        #   }
 
-        loc = []
+        #   response = requests.get('http://www.realestate.ru/scripts/common/ymaps.js', headers=headers).text
+        #   start = response.find("center:") + 9
+        #   ch = ''
+        #   i = 0
+        #   while ch != ']':
+        #       ch = response[start+i]
+        #       i += 1
+        #   end = start + i -1
+        #   loc = response[start:end].split(',')
 
-        return date, cost, descr, pics, room_num, area, adr, metro, contacts, loc
+
+        #print(date, cost, room_num, area, contacts, sep = '\n')
+        return date, cost, descr, pics, room_num, area, metro, contacts
 
         #Location // Можно вытащить из названия объявления
 
@@ -193,46 +212,41 @@ def realestate(maxprice):
 
 
 
+
     def realest(maxprice):
-        
+        #maxprice = 30000
+        p = Parse('realEstate')
         currentPage = 1
         template = 'http://www.realestate.ru'
-        page_url_template = 'http://www.realestate.ru/flatrent/s/rcs10.1.2.3-prt{0}/pg'.format(int(maxprice)//1000)#'http://www.realestate.ru/flatrent/pg'
+        #http://www.realestate.ru/flatrent/s/rcs10.1.2.3-rgs1.2.3.4.5.6.7.8.9.10-prt30/
+        page_url_template = 'http://www.realestate.ru/flatrent/s/rcs10.1.2.3--rgs1.2.3.4.5.6.7.8.9.10-prt{0}/pg'.format(maxprice//1000)#'http://www.realestate.ru/flatrent/pg'
         page_url = page_url_template  + str(currentPage) + '/'
         total_pages = get_total_pages(page_url)+1
-        print("TOTAL PAGES:", total_pages)
         out = []
-
-        for currentPage in range(total_pages)[1:]:
-            p = Parse('realEstate')
+        for currentPage in range(total_pages):
             # if currentPage == 2:
-            #   break
-            print("---CURRENT__PAGE: ", currentPage)
+            #   break   
             page_url = page_url_template  + str(currentPage) + '/'
             soup = BeautifulSoup(get_html(page_url), 'lxml')
             ads = soup.find('section', class_='clear-fix').find_all('div', class_='contentblock')[1].find('div', class_='main-content').find('div', class_='list-panel').find_all('div', class_='obj')
-            for ad in ads:
-                try:
-                    url = template + ad.find('div', class_='obj-item').find('a').get('href')
-                    #lat = ad.find('div', class_='obj-item').find('a', class_='obj-name house-Geoposition').get('lat') # Получаем широту и долготу
-                    #lng = ad.find('div', class_='obj-item').find('a', class_='obj-name house-Geoposition').get('lng') # скрытые в названии объявления
-                    adr = ad.find('div', class_='obj-item').find('a', class_='obj-name house-Geoposition').text
-                    html = get_html(url)
-                    print(url)
-                    date, cost, descr, pics, room_num, area, adr2, metro, contacts, loc = get_page_data(html)
-                    data = {'date': date, 'cost': cost, 'descr': descr, 'pics':pics, 'room_num': room_num, 'area':area, 'adr':adr, 'metro': metro, 'url': url, 'loc': loc, 'contacts':contacts}
-                    p.append(data)
-                    #print(data)
-                    p.write_status(currentPage)
-                    print('Current page: %s' % currentPage)
-                except:
-                    alertExc()
-                    pass
-            del p
 
-        p = Parse('realEstate')
+            for ad in ads:
+                url = template + ad.find('div', class_='obj-item').find('a').get('href')
+                #lat = ad.find('div', class_='obj-item').find('a', class_='obj-name house-Geoposition').get('lat') # Получаем широту и долготу
+                #lng = ad.find('div', class_='obj-item').find('a', class_='obj-name house-Geoposition').get('lng') # скрытые в названии объявления
+                adr = ad.find('div', class_='obj-item').find('a', class_='obj-name house-Geoposition').text
+                #print(adr)
+                html = get_html(url)
+                #print(url)
+                date, cost, descr, pics, room_num, area, metro, contacts= get_page_data(html)
+                data = {'date': date, 'cost': cost, 'descr': descr, 'pics':pics, 'room_num': room_num, 'area':area, 'adr':adr, 'metro': metro, 'url': url, 'contacts':contacts}
+                p.append(data)
+                
+                # with open('out.txt', 'w', encoding='utf-8') as out_file:
+                #   out_file.write(str(out))
+                p.write_status(currentPage)
+                #time.sleep(3)
         p.add_date()
-        del p
 
     # run
     realest(maxprice)
